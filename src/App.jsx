bd793a4de22f4d673adf4fde4bac0b373b9595e3
Alok810 <154756272+Alok810@users.noreply.github.com>
2025-06-28 12:05:29 +0530

    Hello

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import{ useState } from 'react'
+import { useState } from 'react'
 import SideBar from './component/SideBar'
 import { Outlet } from 'react-router-dom'
 
 function App() {
-
   return (
-    <div className='flex box-border bg-gray-800'>
+    <div
+      className="flex box-border min-h-screen"
+      style={{
+        background: 'linear-gradient(to bottom, #e6f4ea 0%, #fce7f3 50%, #e6f4ea 100%)',
+      }}
+    >
       <SideBar />
-      <div className='w-full mx-20 my-4'>
-        {<Outlet/>}
+      <div className="w-full mx-20 my-4">
+        <Outlet />
       </div>
     </div>
   )
