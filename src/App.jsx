556948524bf59cf9401a439d609ec762c3a18492
Zhankrut Verma <zhankrutverma2802@gmail.com>
2025-06-28 11:09:25 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,10 @@ import { Outlet } from 'react-router-dom'
 function App() {
 
   return (
-    <div className='flex box-border bg-gray-800'>
+    <div className='flex box-border bg-gray-800' >
       <SideBar />
       <div className='w-full mx-20 my-4'>
-        {<Outlet/>}
+        {<Outlet />}
       </div>
     </div>
   )
