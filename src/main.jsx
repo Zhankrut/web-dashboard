3df5ee690017ca286c03d74bd0831196c4fbdbf8
Zhankrut Verma <zhankrutverma2802@gmail.com>
2025-07-07 16:02:23 +0530

    debug some of the errors

--- src/main.jsx
+++ src/main.jsx
@@ -1,66 +1,95 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { Home } from './pages/Home.jsx'
-import PasswordManager from './pages/PasswordManager.jsx'
-import VulnerabilityScan from './pages/VulnerabilityScan.jsx'
-import HoneyDashboard from './pages/HoneyPotMonitor.jsx'
-import Profile from './pages/Profile.jsx'
-import Settings from './pages/Settings.jsx'
-import { ClerkProvider } from '@clerk/clerk-react'
-import ProtectedRoute from './component/protectedRoute.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home } from "./pages/Home.jsx";
+import PasswordManager from "./pages/PasswordManager.jsx";
+import VulnerabilityScan from "./pages/VulnerabilityScan.jsx";
+import HoneyPotMonitor from "./pages/HoneyPotMonitor.jsx";
+import Profile from "./pages/Profile.jsx";
+import Settings from "./pages/Settings.jsx";
+import { ClerkProvider } from "@clerk/clerk-react";
+import ProtectedRoute from "./component/protectedRoute.jsx";
+import Signin from "./component/SignIn.jsx";
+import Signup from "./component/SignUp.jsx";
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error('Missing Publishable Key')
+  throw new Error("Missing Publishable Key");
 }
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
     children: [
       {
         index: true,
-        element: <ProtectedRoute><Home /></ProtectedRoute>
+        element: (
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        ),
       },
       {
-        path: '/password-manager',
-        element: <ProtectedRoute><PasswordManager /></ProtectedRoute>
+        path: "password-manager",
+        element: (
+          <ProtectedRoute>
+            <PasswordManager />
+          </ProtectedRoute>
+        ),
       },
       {
-        path: '/vulnerability-scan',
-        element: <ProtectedRoute><VulnerabilityScan /></ProtectedRoute>
+        path: "vulnerability-scan",
+        element: (
+          <ProtectedRoute>
+            <VulnerabilityScan />
+          </ProtectedRoute>
+        ),
       },
       {
-        path: '/honeypot-monitor',
-        element: <ProtectedRoute> <HoneyDashboard /></ProtectedRoute>
+        path: "honeypot-monitor",
+        element: (
+          <ProtectedRoute>
+            <HoneyPotMonitor />
+          </ProtectedRoute>
+        ),
       },
       {
-        path: '/profile',
-        element: <Profile />
-      },
-      {
-        path: '/settings',
-        element: <ProtectedRoute> <Settings /></ProtectedRoute>
-      },
-      // {
-      //   path: 'manager',
-      //   element: <Manager />,
-      // },
+        path: "profile",
 
-    ]
-  }
+        element: (
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        )
+      },
+      {
+        path: "settings",
+        element: (
+          <ProtectedRoute>
+            <Settings />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "login",
+        element: <Signin />,
+      },
+      {
+        path: "signup",
+        element: <Signup />,
+      },
+    ],
+  },
 ]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
       <RouterProvider router={router} />
     </ClerkProvider>
-
-  </StrictMode>,
-)
+  </StrictMode>
+);
