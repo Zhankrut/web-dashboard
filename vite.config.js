49a75721aabc1524ee183e468989e18b04eb1388
Zhankrut Verma <zhankrutverma2802@gmail.com>
2025-07-10 21:18:18 +0530

    make changes to the files

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/zap": {
-        target: "http://localhost:8080", // ✅ correct ZAP API host
+        target: "http://127.0.0.1:8080", // ✅ correct ZAP API host
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/zap/, "/JSON"), // ✅ prepends /JSON
       },
